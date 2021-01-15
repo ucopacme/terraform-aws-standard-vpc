5e9eeca1de29adc1fc4f1e8b2ef14a213f0de6da
Eric Odell <eric.odell@ucop.edu>
2021-01-14 18:34:44 -0800

    routes per zone

--- modules/route_table_associations/main.tf
+++ modules/route_table_associations/main.tf
@@ -1,10 +1,14 @@
 resource "aws_route_table_association" "subnets" {
-  count          = length(var.subnet_ids)
-  subnet_id      = var.subnet_ids[count.index]
+  count     = length(var.subnet_ids)
+  subnet_id = var.subnet_ids[count.index]
+  #for_each = toset(var.subnet_ids)
+  #subnet_id      = each.value
   route_table_id = var.route_table_id
 }
 resource "aws_route_table_association" "gateways" {
-  count          = length(var.gateway_ids)
-  gateway_id     = var.gateway_ids[count.index]
+  count      = length(var.gateway_ids)
+  gateway_id = var.gateway_ids[count.index]
+  #for_each = toset(var.gateway_ids)
+  #gateway_id     = each.value
   route_table_id = var.route_table_id
 }
